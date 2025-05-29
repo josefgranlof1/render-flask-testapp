d82a7caf9df9d93c98fca11926825e22448b13d5
josef <josefgranlof@hotmail.com>
2025-05-29 23:47:50 +0200

    demoapp 39th commit

--- env.py
+++
@@ -1,3 +0,0 @@
-from src.models import UserData, UserPreference, UserImages, Match, Message, RelationshipData, Task
-
-DATABASE_URL= "postgresql://wingsdatingapp7_render_example_v4t6_user:qZkXm6rO0i34IqcQYrBOZ4PcgQxKj23e@dpg-d0o86aje5dus73b85gl0-a.frankfurt-postgres.render.com/wingsdatingapp7_render_example_v4t6"
